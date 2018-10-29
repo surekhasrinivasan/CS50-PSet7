a9aaa39c84539f7591d4f197543e6b50fbc3cde2
Surekha Srinivasan <surekhasrao@gmail.com>
2018-10-29 15:23:12 +0000

    Register User

--- finance/application.py
+++ finance/application.py
@@ -115,7 +115,40 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    return apology("TODO")
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Ensure confirmation password was submitted
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("password does not match", 403)
+
+        # Insert the new user into users, storing the hash of the user's password
+        result = db.execute("INSERT INTO users (username, hash) VALUES(:username, :hash)",
+                            username = request.form.get('username'),
+                            hash = generate_password_hash(request.form.get('password')))
+
+        # Remember which user has logged in
+        session["user_id"] = result
+
+        # Redirect user to home page
+        return redirect("/")
+
+        # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
 
 
 @app.route("/sell", methods=["GET", "POST"])
